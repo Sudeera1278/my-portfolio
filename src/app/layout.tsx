df217d1ea9fff1c068b497cd99d948b25f2dd4db
sudeera amarakoon <sudeeraamarakoon5@gmail.com>
2025-12-06 17:27:15 +0000

    can you get my profile image as a favicon icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="https://i.imgur.com/u8ZxlHD.png" />
       </head>
       <body className="font-body antialiased">
         {children}
