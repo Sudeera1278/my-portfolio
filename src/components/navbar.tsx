19931deaecb2bdc58cef7926328030585e78afa3
sudeera amarakoon <sudeeraamarakoon5@gmail.com>
2025-12-10 11:26:05 +0000

    remove that line (_for element div_)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,6 @@ const Navbar = () => {
   return (
     <header className="absolute top-0 left-0 right-0 z-50 px-4 py-3 md:px-6 md:py-4 bg-transparent">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center gap-2 text-lg font-bold text-white">Sudeera Dilshan</div>
       </div>
     </header>
   );
